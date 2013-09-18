da4dcbf4dd38d14368f6ca5e564451780f80f03b
Richard Davis <rdavis.bacs@gmail.com>
2013-09-17 20:33:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gametoday::Application.config.secret_key_base = '6ec08f3a3c6689e03d5b65bcb2a2bb1bb7b712ab55f044461f308a2669f5bba7b643a10f1a4d54a27286e7b97c6fa85dbcbc73ba984ab02bd2c97ab7af6c3d05'
