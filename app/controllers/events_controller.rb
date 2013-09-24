815791129016ee430994b77b0dc890c20ce4f0c6
Richard Davis <rdavis.bacs@gmail.com>
2013-09-24 13:17:44 -0400

    removed links to favicons

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,7 @@ class EventsController < ApplicationController
     #params[:path]   # user types gametoday.in/boston
     #session[:city]  # we set the city
 
-    @city = params[:city] # validate against a known list of cities
+    @city = event_params[:city] # validate against a known list of cities
     #@city = params[:path]
     #@city ||= session[:city]
     #binding.pry
@@ -21,5 +21,10 @@ class EventsController < ApplicationController
       })
       @events = @query.results
     end
+  end
+
+  protected
+  def event_params
+    params.permit(:city)
   end
 end
